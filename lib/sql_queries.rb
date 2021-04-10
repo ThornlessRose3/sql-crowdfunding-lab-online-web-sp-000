f007559ff92fc344e3a967f65fa4728a57960779
Carrie Jones <c.jessica.jones@gmail.com>
2021-04-10 23:43:13 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -24,7 +24,7 @@ end
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   "SELECT projects.title, projects.funding_goal AS goal, SUM(pledges.amount) AS summed_amount, summed_amount-goal AS amount_over_goal FROM projects
   JOIN pledges ON projects.id = pledges.project_id
-   WHERE amount_over_goal >= 0,
+   WHERE amount_over_goal >= 0
    GROUP BY pledges.project_id;"
 end
 
